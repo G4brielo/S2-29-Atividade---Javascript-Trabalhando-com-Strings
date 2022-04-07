07cd475164d438731b569c81c7da438ee6d5d4a3
G4brielo <brenog4briel0@gmail.com>
2022-04-07 13:38:45 -0300

    Finalizado

---
+++ index.js
@@ -0,0 +1,27 @@
+let primeiroNome = "Breno";
+console.log(primeiroNome);
+let segundoNome = "Gabriel";
+console.log(segundoNome);
+
+console.log("Primeiro nome possui " 
++ primeiroNome.length + " letras");
+
+let preco = 15;
+console.log("Preço inicial " + preco);
+let precoFomatado = "R$15,00";
+console.log("Formato real " + precoFomatado);
+console.log(preco = precoFomatado);
+
+let cumprimento = "Bom Dia";
+let nome = "Paulo";
+let sobrenome = "de Tarcio"
+
+let mensagemCumprimento = cumprimento + " " 
++ nome + " " + sobrenome;
+
+console.log(mensagemCumprimento);
+
+
+
+
+
